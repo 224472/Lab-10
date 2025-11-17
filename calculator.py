9c56da3eaf3b0abe9a04cbfcb45fc304b358f20b
224472 <shanou0601@gmail.com>
2025-11-16 23:58:30 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
 # https://github.com/224472/lab11-MO-NF-
 # Partner 1: [Michael Ou]
 # Partner 2: [Partner finished with someone else]
-
+"""
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
 # First example
-
+import math
 
 def add(a, b): 
     return a+b
